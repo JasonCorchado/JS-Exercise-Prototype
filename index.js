56648ca4c6d06107c9ee77159ff9b840f30dbd3b
Jason Corchado <cpsrider89@gmail.com>
2021-01-27 14:39:42 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -39,11 +39,26 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age){
+   this.name = name;
+   this.age = age;
+   this.stomach = [];      
+  }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length< 10){
+      this.stomach.push(edible);
+    }
   }
  
- 
+ Person.prototype.poop = function(){
+   this.stomach = [];
+ }
+
+ Person.prototype.toString = function(){
+   return `${this.name}, ${this.age}`;
+ }
+
 
   
   
@@ -63,10 +78,26 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon){
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;   
   }
-  
+
+  Car.prototype.fill = function(gallons){
+    this.tank = this.tank + gallons;
+  }
+
+  Car.prototype.drive = function(distance){
+    if(this.tank < (distance / this.milesPerGallon)){
+    let total = this.tank * this.milesPerGallon;
+    this.odometer = this.odometer + total;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+   }
+  }
+
+   
   
   /*
     TASK 3
@@ -75,18 +106,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy){
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;   
+  }
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
   }
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Whenever function is contained globally this is the window object.
+    2. when its followed by . the object before is this.
+    3. when using a constructor function this is referencing to the specific object being constructed. 
+    4. when .call .apply or .bind is being used this is being defined explicitly.
   */
   
   
